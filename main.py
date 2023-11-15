0344801dd86f8461e0ba0194bd7b0eb45f771454
ewhite20 <elladidi@gmail.com>
2023-11-15 16:31:03 -0500

    First draft model

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 import numpy as np
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import KNeighborsClassifier
 
-# Data Formatting
 # Filtered dataset
 filtered = pd.read_csv('./stringcourse.csv',  sep=',',  header=0)
+# Add an id column
+filtered['ID'] = range(0, len(filtered))
+filtered.columns = ['text', 'ID']
+filtered['title'] = filtered['text'].apply(lambda x: str(
+    x).split()[1] + str(x).split()[2] if len(str(x).split()) > 1 else None)
+filtered.columns = ['text', 'ID', 'title']
+tfidf_vectorizer = TfidfVectorizer()
+tfidf_desc = tfidf_vectorizer.fit_transform((filtered['text']))
 
-print(filtered)
+# tfidf
+user_tfidf = tfidf_vectorizer.transform(
+    filtered[filtered['title'] == 'aas2043']['text'])
+cos_similarity_tfidf = map(
+    lambda x: cosine_similarity(user_tfidf, x), tfidf_desc)
+n_neighbors = 10
+print(user_tfidf)
+KNN = NearestNeighbors(n_neighbors=n_neighbors, p=2)
+KNN.fit(tfidf_desc)
+NNs = KNN.kneighbors(user_tfidf, return_distance=True)
+# print(NNs)
+
+# I'm confused if this should be for one example or for all of them???
